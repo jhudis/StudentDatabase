566d7374603179ac8137af3aa9183715f1d9b70b
Justin Hudis <justin.hudis@gmail.com>
2017-06-25 15:57:10 -0400

    Initial commit

---
+++ src/students/Student.java
@@ -0,0 +1,16 @@
+package students;
+
+public class Student {
+
+    String firstName, lastName, email;
+
+    public static void main(String[] args) {
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+}
